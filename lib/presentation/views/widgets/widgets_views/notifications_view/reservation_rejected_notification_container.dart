8284ffb187083610c9d45845baea827cb2cd48ad
Angel Poot <angelpootucan7@.gmail.com>
2024-01-11 23:04:26 -0600

    Change in notificaation files

--- lib/presentation/views/widgets/widgets_views/notifications_view/reservation_rejected_notification_container.dart
+++ lib/presentation/views/widgets/widgets_views/notifications_view/reservation_rejected_notification_container.dart
@@ -2,59 +2,57 @@ import 'package:flutter/material.dart';
 
 //* NOTIFICACIONES DE SOLICITUDES RECHAZADAS
 class ReservationRejectedNotificationContainer extends StatelessWidget {
-  final String messageConst;
+  final String titleNotification;
   final String messageApi;
 
-  const ReservationRejectedNotificationContainer({super.key, required this.messageConst, required this.messageApi});
+  const ReservationRejectedNotificationContainer({
+    super.key, 
+    required this.titleNotification, 
+    required this.messageApi
+  });
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        alignment: Alignment.center,
-        height: size.height * .30,
-        width: size.width * .34,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Colors.white, 
-            border: const Border(
-              top: BorderSide(color: Colors.grey, width: .5),
-              bottom: BorderSide(color: Colors.grey, width: .5),
-              left: BorderSide(color: Colors.grey, width: .5),
-              right: BorderSide(color: Colors.grey, width: .5)
-            ),
-        ),
-        child: Row(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(13),
-              height: size.height * .30,
-              width: size.width * .85,
-              child: const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'El apartartamento ya no se encuentra disponible pero aquí te dejamos algunas publicaciones que te pueden gustar ',
-                    style: TextStyle(
-                      color: Colors.black,
-                    ),
-                  ),
-                  Text(
-                    'Link de Publicacion 1',
-                    style: TextStyle(
-                      fontStyle: FontStyle.italic,
-                      color: Colors.red
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      height: size.height * .30,
+      width: size.width * .34,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: const Border(
+            top: BorderSide(color: Colors.grey, width: .5),
+            bottom: BorderSide(color: Colors.grey, width: .5),
+            left: BorderSide(color: Colors.grey, width: .5),
+            right: BorderSide(color: Colors.grey, width: .5)),
+        color: Colors.white,
       ),
-    );  
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(13),
+            height: size.height * .30,
+            width: size.width * .85,
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'El apartartamento ya no se encuentra disponible pero aquí te dejamos algunas publicaciones que te pueden gustar ',
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+                Text(
+                  'Link de Publicacion 1',
+                  style:
+                      TextStyle(fontStyle: FontStyle.italic, color: Colors.red),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
