c498e553d7e589d17fa6d37afffb2387b83a79ce
Gustavo Canul <gusssy12@gmail.com>
2024-01-11 19:36:37 -0600

    advancced of accepted notifications

--- lib/presentation/views/home_views/notificatino_view.dart
+++ lib/presentation/views/home_views/notificatino_view.dart
@@ -1,35 +1,121 @@
-import 'package:studenthive/presentation/screens/widgets/widgets_views/notifications/notification_container_1.dart';
-import 'package:studenthive/presentation/screens/widgets/widgets_views/notifications/notification_container_2.dart';
-import 'package:studenthive/presentation/screens/widgets/widgets_views/notifications/notification_container_3.dart';
 import 'package:flutter/material.dart';
+import 'package:studenthive/presentation/views/widgets/notifications/accepted_notification_container.dart';
+
+//! test user entity
+class User {
+  final int userId;
+  final int contactNumber;
+  final String firstName;
+  final String lastName;
+  final String urlPhoto;
+
+  User({
+    required this.contactNumber,
+    required this.firstName,
+    required this.lastName,
+    required this.urlPhoto,
+    required this.userId,
+  });
+}
+
+//? Enumeración para los tipos de notificaciones
+enum NotificationType { reserve, accepted, notAccepted }
+
+//! test notification entity
+class Notification {
+  final User user;
+  final String title;
+  final int notificationTime;
+  final NotificationType type;
+
+  Notification({
+    required this.user,
+    required this.title,
+    required this.notificationTime,
+    required this.type,
+  });
+}
+
 
 class NotificationView extends StatelessWidget {
-  final String messageConst;
-  final String messageApi;
-
-  const NotificationView({Key? key, required this.messageConst, required this.messageApi})
-      : super(key: key);
+  const NotificationView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        actions: const[
+          //?Esto deajalo así luego vemos que ponemos XD
+          Text(
+            'asdf',
+            style: TextStyle(
+              color: Colors.black87
+            ),
+            ),
+        ]
+      ),
 
-    return ListView(
-      children: <Widget>[
-        SizedBox(),
-        NotificationContainer1(
-          messageConst: messageConst,
-          messageApi: messageApi,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: ListView(
+          children: [
+
+            const Text(
+              'Notifications',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+
+            const SizedBox( height: 40,),
+
+            ...notificationList.map(
+              (notification) => AcceptedNotificationContainer(
+                titleNotification: notification.title,
+                user: notification.user,
+              ),
+            ),
+          ],
         ),
-        NotificationContainer2(
-          messageConst: messageConst,
-          messageApi: messageApi,
-        ),
-        NotificationContainer3(
-          messageConst: messageConst, 
-          messageApi: messageApi
-        ),
-        // ? Agrega más instancias según sea necesario
-      ],
+      ),
     );
   }
 }
+
+//? usuaremos un gestor de estado para hacer un get de publicaciones. 
+List<Notification> notificationList = [
+  Notification(
+    user: userList[0  /* 'esto estará de acuerdo a su idUser, para saber que usuario es' */  ],
+    title: 'Accepted Notification',
+    notificationTime: 1,
+    type: NotificationType.accepted,
+  ),
+  Notification(
+    user: userList[1],
+    title: 'Accepted Notification',
+    notificationTime: 1,
+    type: NotificationType.accepted,
+  ),
+];
+
+//? usuaremos un gestor de estado para hacer un get de usuarios
+List<User> userList = [
+  User(
+    userId: 1,
+    contactNumber: 9999204857, 
+    firstName: 'Gustavo', 
+    lastName: 'Canul', 
+    urlPhoto: 'https://scontent.fmid2-1.fna.fbcdn.net/v/t39.30808-6/262531647_1775392782659993_5430604383254748736_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=dd5e9f&_nc_eui2=AeGI6LsoBo0sP1mlu5ispk6illY1StML69SWVjVK0wvr1FYu4BNbn47F0IKTzxas49JTQyYd_0chbB0bd3M6VB_w&_nc_ohc=TpCOEX9YRJ0AX-Mg31M&_nc_ht=scontent.fmid2-1.fna&oh=00_AfBvQuI30-4kqdmdsPSP0P4VUBDyFTjV4ryZty6WAhIvGw&oe=65A4D625'
+    ),
+  User(
+    userId: 2,
+    contactNumber: 9999204857, 
+    firstName: 'Yahir', 
+    lastName: 'Poot', 
+    urlPhoto: 'https://scontent.fmid2-1.fna.fbcdn.net/v/t39.30808-6/262531647_1775392782659993_5430604383254748736_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=dd5e9f&_nc_eui2=AeGI6LsoBo0sP1mlu5ispk6illY1StML69SWVjVK0wvr1FYu4BNbn47F0IKTzxas49JTQyYd_0chbB0bd3M6VB_w&_nc_ohc=TpCOEX9YRJ0AX-Mg31M&_nc_ht=scontent.fmid2-1.fna&oh=00_AfBvQuI30-4kqdmdsPSP0P4VUBDyFTjV4ryZty6WAhIvGw&oe=65A4D625'
+    )
+];
---
+++ lib/presentation/views/widgets/notifications/accepted_notification_container.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:studenthive/presentation/views/home_views/notificatino_view.dart';
+
+//* NOTIFICACIÓN DE SOLICITUDES ACEPATADAS
+class AcceptedNotificationContainer extends StatelessWidget {
+  final String titleNotification;
+  final User user;
+
+  const AcceptedNotificationContainer({super.key, required this.titleNotification, required this.user,});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      height:size.height * .10,
+      width: size.width * .34,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: const Border(
+          top: BorderSide(color: Colors.grey, width: .5),
+          bottom: BorderSide(color: Colors.grey, width: .5),
+          left: BorderSide(color: Colors.grey, width: .5),
+          right: BorderSide(color: Colors.grey, width: .5)
+        ),
+        color: Colors.white
+      ),
+      child: Row(
+        children:[
+          const Padding(
+            padding: EdgeInsets.all(5.0) 
+          ),
+          
+          //? Es el contendor de la imagen
+          Container(
+            height: size.height * .15,
+            width: size.width * 0.29 * 0.60,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.grey
+            ),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                user.urlPhoto
+              ),
+              ),
+            ),
+          
+          Container(//?InformationContainer
+            padding: const EdgeInsets.all(10),
+            height: size.height * .10,
+            width: size.width * .67,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+
+                Text(
+                  //! Se cambia por el numero de telefono del usuario
+                  titleNotification,
+                  style: const TextStyle(
+                    fontStyle: FontStyle.italic,
+                    color: Colors.red
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
