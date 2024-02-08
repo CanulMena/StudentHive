aa7db3cb91699bead79c2b07b48eb4736cab9345
Angel Poot <angelpootucan7@.gmail.com>
2024-02-07 23:48:25 -0600

    Union de la ramas

--- lib/config/themes/theme_app.dart
+++ lib/config/themes/theme_app.dart
@@ -1,25 +1,21 @@
 import 'package:flutter/material.dart';
 
-const Color mainColor = Color.fromARGB(133, 2, 18, 164);
-List<Color> listColors = [
-  mainColor, 
-  Colors.cyan,
-  Colors.red,
-  Colors.yellow,
-  Colors.green, 
-
-];
+// List<Color> listColors = [
+//   Colors.cyan,
+//   Colors.white
+// ];
 
 class ThemesApp {
   // final int selectedColor;
 
-  ThemesApp({required this.selectedColor})
- : assert( selectedColor >= 0 && selectedColor <= listColors.length -1, 'selectColor must be between 0 and ${listColors.length -1}');
+  // ThemesApp({required this.selectedColor})
+  // : assert( selectedColor >= 0 && selectedColor <= listColors.length -1, 'selectColor must be between 0 and ${listColors.length -1}');
 
-  ThemeData themes() {
+  ThemeData themes(){
     return ThemeData(
-      useMaterial3: false,
-      colorSchemeSeed: listColors[selectedColor],
+      useMaterial3: true,
+      colorSchemeSeed:  Colors.yellow/* listColors[selectedColor] */,
     );
   }
+  
 }
