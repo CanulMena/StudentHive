bc85802c6f7b20c794d035d9d8ef224dd03dca74
Gustavo Canul <gusssy12@gmail.com>
2024-03-24 21:55:22 -0600

    errors fixed

--- lib/presentation/screens/widgets/publication/customlistview_publication.dart
+++ lib/presentation/screens/widgets/publication/customlistview_publication.dart
@@ -25,7 +25,7 @@ class _CustomListViewState extends ConsumerState<CustomListView> {
   @override
   Widget build(BuildContext context) {
     final Map<String, dynamic> userMapDetails = ref.watch(userDetailProvider);
-    final User userDetail = userMapDetails[widget.houseDetail.idUser.toString()];
+    final userDetail = userMapDetails[widget.houseDetail.idUser.toString()];
     return CustomScrollView(
       slivers: [
         CustomSliverAppBar( images: widget.houseDetail.images, ),
@@ -46,7 +46,7 @@ class _CustomListViewState extends ConsumerState<CustomListView> {
 
 class _RentalHouseDetils extends StatelessWidget {
   final House houseDetail;
-  final User userDetail;
+  final userDetail;
   const _RentalHouseDetils({required this.houseDetail, required this.userDetail});
 
   @override
